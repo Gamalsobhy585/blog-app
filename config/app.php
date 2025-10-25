3fc73fbb204c8df1d8b3c25eace0ca4745449f64
Gamalsobhy585 <eng.gamalsobhi@gmail.com>
2025-10-25 16:51:22 +0300

    add memecached

--- config/app.php
+++ config/app.php
@@ -123,4 +123,24 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
+    'memcached' => [
+    'driver' => 'memcached',
+    'persistent_id' => env('MEMCACHED_PERSISTENT_ID'),
+    'sasl' => [
+        env('MEMCACHED_USERNAME'),
+        env('MEMCACHED_PASSWORD'),
+    ],
+    'options' => [
+        // Memcached::OPT_CONNECT_TIMEOUT => 2000,
+    ],
+    'servers' => [
+        [
+            'host' => env('MEMCACHED_HOST', '127.0.0.1'),
+            'port' => env('MEMCACHED_PORT', 11211),
+            'weight' => 100,
+        ],
+    ],
+],
+
+
 ];
