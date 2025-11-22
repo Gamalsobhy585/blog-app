cf0a180f61b71af0db1452a1e0ee509bed15d738
Gamalsobhy585 <eng.gamalsobhi@gmail.com>
2025-11-22 16:16:09 +0300

    create author factory

--- database/factories/AuthorFactory.php
+++ database/factories/AuthorFactory.php
@@ -2,22 +2,24 @@
 
 namespace Database\Factories;
 
+use App\Models\Author;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Author>
- */
 class AuthorFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = Author::class;
+
     public function definition(): array
     {
         return [
-            //
+            'uuid'         => (string) Str::uuid(),
+            'name'         => $this->faker->name(),
+            'bio'          => $this->faker->optional()->paragraph(),
+            'nationality'  => $this->faker->optional()->country(),
+            'is_approved'  => $this->faker->boolean(60), 
+            'created_by' => \App\Models\User::inRandomOrder()->value('id'),
+            
         ];
     }
 }
